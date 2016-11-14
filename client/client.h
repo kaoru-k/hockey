4eb5d434f972c13db82bfd3ef1a5ba23f875e6d4
kaoru-k <kaoru_kimura@outlook.com>
2016-11-14 12:34:26 +0900

    client.hを作成

---
+++ client/client.h
@@ -0,0 +1,19 @@
+#include "../common.h"
+
+/***** 定数 *****/
+#define PAD_H 5
+
+/***** グローバル変数 *****/
+PLAYER p[6]; //0,1,2,3->human 4,5->cpu
+PAD pad;
+
+/***** 外部関数 *****/
+// client_net.c
+void client_server(char *server_name, u_short port);
+void recv_data(void *data, int size);
+void send_data(void *data, int size);
+void terminate_client(void);
+
+// client_game.c
+
+// client_ui.c
