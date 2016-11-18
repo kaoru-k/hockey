af70a929c750eb00e8d1eaf135406fcb51756698
kaoru-k <kaoru_kimura@outlook.com>
2016-11-18 15:04:00 +0900

    サーバ周り

--- server/server_main.c
+++ server/server_main.c
@@ -6,8 +6,30 @@
 
 #include "server.h"
 
-int main(int argc, char** args)
+int main(int argc, char *argv[])
 {
-    
+    u_short port = DEFAULT_PORT;
+    int flag = 1;
+
+    switch(argc) {
+    case 1:
+        break;
+    case 2:
+        port = atoi(argv[1]);
+        break;
+    default:
+        fprintf(stderr, "Too many argument!\n");
+        return 1;
+    }
+
+    fprintf(stderr, "Port number = %d\n", port);
+
+    setup_server(port);
+
+    while(flag) {
+    }
+
+    terminate_server();
+
     return 0;
 }
--- server/server_net.c
+++ server/server_net.c
@@ -8,7 +8,51 @@
 
 void setup_server(u_short port)
 {
+    int rsock, sock = 0;
+    struct sockaddr_in sv_addr, cl_addr;
 
+    fprintf(stderr, "setup_server() started\n");
+
+    /* リクエスト用ソケットを生成 */
+    fprintf(stderr, "Generating request socket ...");
+    rsock = socket(AF_INET, SOCK_STERAM, 0);
+    if (rsock < 0) error_message("failed!\n");
+    fprintf(stderr, "done.\n");
+
+    /* ソケットの基本設定 */
+    sv_addr.sin_family = AF_INET;
+    sv_addr.sin_port = htons(port);
+    sv_addr.sin_addr.s_addr = INADDR_ANY;
+    int opt = 1;
+    setsockopt(rsock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    /* ソケットに設定をbindする */
+    fprintf(stderr, "bind() ...");
+    if (listen(rsock, 4) != 0) error_message("failed!\n");
+    fprintf(stderr, "done.\n");
+
+    /* 接続ソケットの準備 */
+    fprint(stderr, "listen() ...");
+    if (listen(rsock, 4) != 0) error_message("failed!\n");
+    fprintf(stderr, "started.\n");
+
+    /* クライアントからの接続を受け付ける */
+    int i, max_sock = 0;
+    socklen_t len;
+    char src[MAX_LEN_ADDR];
+    for (i = 0; i < num_clients; i++) {
+        len = sizeof(cl_addr);
+        sock = accept(rsock, (struct sockaddr *)&cl_addr, &len);
+
+        if (sock < 0) error_message("accept() failed!\n");
+        if (max_sock < sock) max_sock = sock;
+        clients[i].cid = i;
+        clients[i].sock = sock;
+        clients[i].addr = cl_addr;
+        memset(src, 0, sizeof(src));
+        inet_ntop(AF_INET, (struct sockaddr *)&cl_addr.sin_addr, src, sizeof(src));
+
+        fprintf(stderr, 
 }
 
 void recv_data(int cid, void *data, int size)
@@ -24,4 +68,10 @@ void send_data(int cid, void *data, int size)
 void terminate_server(void);
 {
 
+}
+
+void error_message(char *message)
+{
+    perror(message);
+    exit(1);
 }
