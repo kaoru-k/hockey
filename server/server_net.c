6c46e1b09c75e23d7b33ec47e0c6db185a50b559
kaoru-k <kaoru_kimura@outlook.com>
2016-11-18 16:51:23 +0900

    ネットワーク処理

--- server/server_net.c
+++ server/server_net.c
@@ -40,7 +40,7 @@ void setup_server(u_short port)
     int i, max_sock = 0;
     socklen_t len;
     char src[MAX_LEN_ADDR];
-    for (i = 0; i < num_clients; i++) {
+    for (i = 0; i < 4; i++) {
         len = sizeof(cl_addr);
         sock = accept(rsock, (struct sockaddr *)&cl_addr, &len);
 
@@ -52,12 +52,26 @@ void setup_server(u_short port)
         memset(src, 0, sizeof(src));
         inet_ntop(AF_INET, (struct sockaddr *)&cl_addr.sin_addr, src, sizeof(src));
 
-        fprintf(stderr, 
+        fprintf(stderr, "Client %d is accepted (address:%s port:%d).\n", i, src, ntohs(cl_addr.sin_port));
+    }
+
+    close(rsock);
+
+    num_socks = max_sock + 1;
+    FD_ZERO(&mask);
+    FD_SET(0, &mask);
+
+    for(i = 0; i < 4; i++){
+        FD_SET(clients[i].sock, &mask);
+    }
+    fprintf(stderr, "Server setup is done.\n");
 }
 
 void recv_data(int cid, void *data, int size)
 {
-
+    if ((cid != BROADCAST) && (0 > cid || cid >= 4))
+        error_message("recv_data(): Client ID is wrong!\n");
+    
 }
 
 void send_data(int cid, void *data, int size)
