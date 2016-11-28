275ba43b3121ba9c5d167c90127dffa8569c0ae8
nagachan2 <37231003com8@gmail.com>
2016-11-28 17:23:04 +0900

    main2.cのdef_ugoki完成

--- practice/main2.c
+++ practice/main2.c
@@ -86,7 +86,7 @@ int def_ugoki(int i){
     a = 0;
     if(pad.speed_x < 0)
         b = -1;
-    a = (DEF_Y - pad.y - PAD_R)/pad.speed_y * pad.speed_x + pad.x;
+    a = (-i*DEF_Y - pad.y + i*PAD_R)/pad.speed_y * pad.speed_x + pad.x;
     if(a - PAD_R <= FIELD_W && PAD_R + a >= FIELD_W){
         return a;
     }else{
