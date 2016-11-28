77b0ba584278125aadbfb357dda79be6a37c2e91
kaoru-k <kaoru_kimura@outlook.com>
2016-11-28 15:58:35 +0900

    つながるようになった！

--- client/client_net.c
+++ client/client_net.c
@@ -15,7 +15,7 @@ static int myid;
 static int sock;
 static int num_sock;
 static fd_set mask;
-
+CLIENT clients[4];
 
 static void error_message(char *message);
 
@@ -24,39 +24,40 @@ void setup_client(char *server_name, u_short port)
     struct hostent *server;
     struct sockaddr_in sv_addr;
 
-    fprintf(stderr, "Connecting server(name: %s, port: %d)... ", server_name, port);
-    if ((server = gethostbyname(server_name)) == NULL) error_message("gethostbyname() failed!");
+    fprintf(stderr, "Connecting to server (name = %s, port = %d)...", server_name, port);
+    if ((server = gethostbyname(server_name)) == NULL) error_message("failed!\ngethostbyname()");
     
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) error_message("socket() failed!");
-
+    if (sock < 0) {
+        error_message("failed!\nsocket()");
+    }
+    
     sv_addr.sin_family = AF_INET;
     sv_addr.sin_port = htons(port);
     sv_addr.sin_addr.s_addr = *(u_int *)server->h_addr_list[0];
 
-    if (connect(sock, (struct sockaddr *)&sv_addr, sizeof(sv_addr)) != 0) error_message("connect() failed!");
+    if(connect(sock, (struct sockaddr *)&sv_addr, sizeof(sv_addr)) != 0) error_message("failed!\nconnect()");
     fprintf(stderr, "done.\n");
 
     fprintf(stderr, "Waiting for other clients... ");
     recv_data(&myid, sizeof(int));
-    fprintf(stderr, "done.\nYou are %dP\n", myid);
-
-    FD_ZERO(&mask);
-    FD_SET(0, &mask);
-    FD_SET(sock, &mask);
-    fprintf(stderr, "Client setup is done.");
+    fprintf(stderr, "done.\nYour ID = %d.\n", myid);
+    int i;
+    for(i = 0; i < 4; i++) {
+        recv_data(&clients[i], sizeof(CLIENT));
+    }
 }
 
 void recv_data(void *data, int size)
 {
-    if ((data == NULL) || (size <= 0)) error_message("recv_data() failed!");
+    if ((data == NULL) || (size <= 0)) error_message("recv_data()");
     read(sock, data, size);
 }
 
 void send_data(void *data, int size)
 {
-    if ((data == NULL) || (size <= 0)) error_message("send_data() failed!");
-    if (write(sock, data, size) == -1) error_message("send_data() failed!");
+    if ((data == NULL) || (size <= 0)) error_message("send_data()");
+    if (write(sock, data, size) == -1) error_message("send_data()");
 }
 
 void terminate_client(void)
--- server/server_net.c
+++ server/server_net.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 void setup_server(u_short port);
@@ -20,33 +21,28 @@ void setup_server(u_short port)
 {
     int rsock, sock = 0;
     struct sockaddr_in sv_addr, cl_addr;
-
-    fprintf(stderr, "setup_server() started\n");
-
-    /* リクエスト用ソケットを生成 */
+    
+    fprintf(stderr, "setup_server() started.\n");
+    
     fprintf(stderr, "Generating request socket ...");
-    rsock = socket(AF_INET, SOCK_STREAM, 0);
-    if (rsock < 0) error_message("failed!\n");
+    rsock = socket(AF_INET, SOCK_STREAM, 0); 
+    if (rsock < 0) error_message("failed!\nsocket()");
     fprintf(stderr, "done.\n");
 
-    /* ソケットの基本設定 */
     sv_addr.sin_family = AF_INET;
     sv_addr.sin_port = htons(port);
     sv_addr.sin_addr.s_addr = INADDR_ANY;
     int opt = 1;
     setsockopt(rsock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    /* ソケットに設定をbindする */
+    
     fprintf(stderr, "bind() ...");
-    if (listen(rsock, 4) != 0) error_message("failed!\n");
+    if (bind(rsock, (struct sockaddr *)&sv_addr, sizeof(sv_addr)) != 0) error_message("failed!\nbind()");
     fprintf(stderr, "done.\n");
 
-    /* 接続ソケットの準備 */
     fprintf(stderr, "listen() ...");
-    if (listen(rsock, 4) != 0) error_message("failed!\n");
+    if (listen(rsock, 4) != 0) error_message("failed!\nlisten()");
     fprintf(stderr, "started.\n");
 
-    /* クライアントからの接続を受け付ける */
     int i, max_sock = 0;
     socklen_t len;
     char src[MAX_LEN_ADDR];
@@ -54,24 +50,26 @@ void setup_server(u_short port)
         len = sizeof(cl_addr);
         sock = accept(rsock, (struct sockaddr *)&cl_addr, &len);
 
-        if (sock < 0) error_message("accept() failed!\n");
+        if (sock < 0) error_message("accept()");
+        
         if (max_sock < sock) max_sock = sock;
+
         clients[i].cid = i;
         clients[i].sock = sock;
         clients[i].addr = cl_addr;
         memset(src, 0, sizeof(src));
         inet_ntop(AF_INET, (struct sockaddr *)&cl_addr.sin_addr, src, sizeof(src));
 
-        fprintf(stderr, "Client %d is accepted (address:%s port:%d).\n", i, src, ntohs(cl_addr.sin_port));
+        fprintf(stderr, "Client %d is accepted (address=%s, port=%d).\n", i, src, ntohs(cl_addr.sin_port));
     }
-
+    
     close(rsock);
-
+    
     num_socks = max_sock + 1;
     FD_ZERO(&mask);
     FD_SET(0, &mask);
-
-    for(i = 0; i < 4; i++){
+    
+    for(i = 0; i < 4; i++) {
         FD_SET(clients[i].sock, &mask);
     }
     fprintf(stderr, "Server setup is done.\n");
